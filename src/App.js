e006609c85278fb9ae20d0cafe79cab962a52a61
PatOn <mahomed2278@gmail.com>
2021-03-17 21:22:12 +0000

    button handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import classes from "./App.css";
 import TBodyElement from "./Components/Table/ScoreTableCreator/ScoreTableCreator";
-
+import React from "react";
 //data import
 import allCountryScores from "./data/scores";
 
@@ -29,17 +29,75 @@ modifiedDataCountryScores.sort((a, b) => {
   if (a.name[0] > b.name[0]) return 1;
   else return 0;
 });
+let controllerCountries = true;
+let controllerScores = true;
 
 function App() {
+  const [dataCountryScore, dataCountryScoreHandler] = React.useState(
+    modifiedDataCountryScores
+  );
+
+  const sortResultsHandler = () => {
+    if (!controllerScores) {
+      const dataSort = [...dataCountryScore];
+      dataSort.forEach((item) => {
+        //sorting the array
+        item.scores.sort((a, b) => {
+          if (a.s < b.s) return 1;
+          if (a.s > b.s) return -1;
+          else return 0;
+        });
+      });
+      controllerScores = !controllerScores;
+      return dataCountryScoreHandler(dataSort);
+    } else {
+      const dataSort = [...dataCountryScore];
+      dataSort.forEach((item) => {
+        //sorting the array
+        item.scores.sort((a, b) => {
+          if (a.s < b.s) return -1;
+          if (a.s > b.s) return 1;
+          else return 0;
+        });
+      });
+      controllerScores = !controllerScores;
+      return dataCountryScoreHandler(dataSort);
+    }
+  };
+
+  const sortCountriesHandler = (e) => {
+    if (!controllerCountries) {
+      const dataSort = [...dataCountryScore];
+      dataSort.sort((a, b) => {
+        if (a.name[0] < b.name[0]) return -1;
+        if (a.name[0] > b.name[0]) return 1;
+        else return 0;
+      });
+      controllerCountries = !controllerCountries;
+      return dataCountryScoreHandler(dataSort);
+    } else {
+      const dataSort = [...dataCountryScore];
+      dataSort.sort((a, b) => {
+        if (a.name[0] < b.name[0]) return 1;
+        if (a.name[0] > b.name[0]) return -1;
+        else return 0;
+      });
+      controllerCountries = !controllerCountries;
+      return dataCountryScoreHandler(dataSort);
+    }
+  };
+
   return (
     <div className="App">
       <header className="">
         <h1>High Score Tables (React)</h1>
       </header>
       <div className={`blueBorder`}>
+        <button onClick={sortCountriesHandler}>Sort By Countries</button>
+        <button onClick={sortResultsHandler}>Sort By Scores</button>
         <h2>High Scores per Country</h2>
 
-        <TBodyElement dataScoresPlayers={modifiedDataCountryScores} />
+        <TBodyElement dataScoresPlayers={dataCountryScore} />
 
         {/* </main> */}
       </div>
